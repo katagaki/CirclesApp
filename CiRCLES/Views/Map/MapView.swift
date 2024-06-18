ab1ae4f5f709b1f9347fb442c61d176d8d0d3823
katagaki <68231035+katagaki@users.noreply.github.com>
2024-06-18 21:55:40 +0900

    Use MapView

---
+++ CiRCLES/Views/Map/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  CiRCLES
+//
+//  Created by シン・ジャスティン on 2024/06/18.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Image(systemName: "map.fill")
+    }
+}
+
+#Preview {
+    MapView()
+}
