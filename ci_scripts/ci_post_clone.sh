14229ee84dc6f8de40a028735b3e32fe58213012
katagaki <68231035+katagaki@users.noreply.github.com>
2024-07-09 22:59:46 +0900

    Create OpenID plist using Xcode Cloud build script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  CiRCLES
+#
+#  Created by シン・ジャスティン on 2024/07/09.
+#  
+
+OPENID_PLIST_PATH="OpenID.plist"
+
+/usr/libexec/PlistBuddy -c "Clear dict" $OPENID_PLIST_PATH || /usr/libexec/PlistBuddy -c "Add : dict" $OPENID_PLIST_PATH
+
+/usr/libexec/PlistBuddy -c "Add :client_id string $OPENID_CLIENT_ID" $OPENID_PLIST_PATH
+/usr/libexec/PlistBuddy -c "Add :client_secret string $OPENID_CLIENT_SECRET" $OPENID_PLIST_PATH
+/usr/libexec/PlistBuddy -c "Add :client_redirect_url string $OPENID_CLIENT_REDIRECT_URL" $OPENID_PLIST_PATH
+
+echo "OpenID.plist created at $OPENID_PLIST_PATH"
