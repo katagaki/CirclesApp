35cf45f42bb27b6c9cae43865e18cc960449f3c4
katagaki <68231035+katagaki@users.noreply.github.com>
2024-07-21 14:09:47 +0900

    Support loading genres

---
+++ Shared/Data Models/ComiketGenre.swift
@@ -0,0 +1,33 @@
+//
+//  ComiketGenre.swift
+//  CiRCLES
+//
+//  Created by シン・ジャスティン on 2024/07/21.
+//
+
+import SQLite
+import SwiftData
+
+@Model
+final class ComiketGenre: SQLiteable {
+    var eventNumber: Int
+    var id: Int
+    var name: String
+    var code: Int
+    var day: Int
+
+    init(from row: Row) {
+        let colNumber = Expression<Int>("comiketNo")
+        let colID = Expression<Int>("id")
+        let colName = Expression<String>("name")
+        let colCode = Expression<Int>("code")
+        let colDay = Expression<Int>("day")
+
+        self.eventNumber = row[colNumber]
+        self.id = row[colID]
+        self.name = row[colName]
+
+        self.code = row[colCode]
+        self.day = row[colDay]
+    }
+}
