e610606dbdba30c9358b6c08a4b5df24c8ba9cde
katagaki <68231035+katagaki@users.noreply.github.com>
2024-08-16 00:18:49 +0900

    Support navigating to circle detail view from map

--- CiRCLES/Views/Map/InteractiveMapDetailPopover.swift
+++ CiRCLES/Views/Map/InteractiveMapDetailPopover.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct InteractiveMapDetailPopover: View {
+
+    @EnvironmentObject var navigationManager: NavigationManager
+    @Environment(DatabaseManager.self) var database
+
+    @Binding var isPresented: Bool
     @Binding var circles: [ComiketCircle]
 
     var body: some View {
-        List(circles, id: \.id) { circle in
-            Text(circle.circleName)
+        VStack(spacing: 8.0) {
+            ForEach(circles, id: \.id) { circle in
+                Button {
+                    isPresented = false
+                    navigationManager.push(.circlesDetail(circle: circle), for: .map)
+                } label: {
+                    HStack {
+                        if let circleImage = database.circleImage(for: circle.id) {
+                            Image(uiImage: circleImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 72.0)
+                        }
+                        Text(circle.circleName)
+                    }
+                    .contentShape(.rect)
+                }
+                .buttonStyle(.plain)
+            }
         }
-        .frame(minWidth: 300.0, maxWidth: .infinity, minHeight: 300.0, maxHeight: .infinity)
+        .padding()
         .presentationCompactAdaptation(.popover)
     }
 }
