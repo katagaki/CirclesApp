d9144e3cd52f7842fd3b9762afa94c3ec02d2e26
katagaki <68231035+katagaki@users.noreply.github.com>
2024-08-16 13:47:23 +0900

    Implement search in Circles view

--- CiRCLES/Views/Circles/CirclesView.swift
+++ CiRCLES/Views/Circles/CirclesView.swift
@@ -18,7 +18,11 @@ struct CirclesView: View {
     let gridSpacing: CGFloat = 1.0
 
     @State var displayedCircles: [ComiketCircle] = []
+    @State var searchedCircles: [ComiketCircle]?
+
     @State var selectedBlock: ComiketBlock?
+    @State var selectedDate: ComiketDate?
+
     @State var searchTerm: String = ""
 
     var body: some View {
@@ -35,7 +39,7 @@ struct CirclesView: View {
                 LazyVGrid(columns: UIDevice.current.userInterfaceIdiom == .phone ?
                           phoneColumnConfiguration : padOrMacColumnConfiguration,
                           spacing: gridSpacing) {
-                    ForEach(displayedCircles) { circle in
+                    ForEach(searchedCircles == nil ? displayedCircles : searchedCircles ?? []) { circle in
                         NavigationLink(value: ViewPath.circlesDetail(circle: circle)) {
                             if let image = database.circleImage(for: circle.id) {
                                 Image(uiImage: image)
@@ -48,44 +52,38 @@ struct CirclesView: View {
                     }
                 }
             }
+            .navigationTitle("ViewTitle.Circles")
+            .navigationBarTitleDisplayMode(.inline)
             .toolbarBackground(.hidden, for: .tabBar)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button("Shared.Filter", systemImage: "line.3.horizontal.decrease.circle") {
-                        // TODO: Implement filters
-                    }
-                }
-            }
             .safeAreaInset(edge: .bottom, spacing: 0.0) {
                 BarAccessory(placement: .bottom) {
                     ScrollView(.horizontal) {
                         HStack(spacing: 12.0) {
-                            Menu {
+                            BarAccessoryMenu("Shared.Block", icon: "table.furniture") {
                                 ForEach(database.eventMaps, id: \.id) { map in
-                                    Section(map.name) {
+                                    Picker(selection: $selectedBlock) {
                                         ForEach(database.blocks(in: map), id: \.id) { block in
-                                            Button(block.name) {
-                                                selectedBlock = block
-                                            }
+                                            Text(block.name)
+                                                .tag(block)
                                         }
+                                    } label: {
+                                        Text(map.name)
                                     }
+                                    .pickerStyle(.menu)
                                 }
-                            } label: {
-                                HStack(spacing: 8.0) {
-                                    Image(systemName: "table.furniture")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 18.0, height: 18.0)
-                                    Text("Shared.Block")
-                                        .fontWeight(.medium)
-                                }
-                                .foregroundStyle(.white)
-                                .padding([.top, .bottom], 12.0)
-                                .padding([.leading, .trailing], 16.0)
-                                .background(.accent)
                             }
-                            .buttonStyle(.plain)
-                            .clipShape(.capsule)
+                            BarAccessoryMenu("Shared.Day", icon: "calendar") {
+                                Picker(selection: $selectedDate) {
+                                    Text("Shared.All")
+                                        .tag(nil as ComiketDate?)
+                                    ForEach(database.eventDates, id: \.id) { date in
+                                        Text("Shared.\(date.id)th.Day")
+                                            .tag(date)
+                                    }
+                                } label: {
+                                    Text("Shared.Day")
+                                }
+                            }
                         }
                         .padding([.leading, .trailing], 12.0)
                         .padding([.top, .bottom], 12.0)
@@ -93,16 +91,15 @@ struct CirclesView: View {
                     .scrollIndicators(.hidden)
                 }
             }
-            .searchable(text: $searchTerm,
-                        placement: .navigationBarDrawer(displayMode: .always))
-            .onChange(of: selectedBlock) { _, newValue in
-                withAnimation(.snappy.speed(2.0)) {
-                    if let newValue {
-                        displayedCircles = database.circles(in: newValue).sorted(by: {$0.id < $1.id})
-                    } else {
-                        displayedCircles.removeAll()
-                    }
-                }
+            .searchable(text: $searchTerm, placement: .navigationBarDrawer(displayMode: .always), prompt: "Circles.Search.Prompt")
+            .onChange(of: selectedBlock) { _, _ in
+                reloadDisplayedCircles()
+            }
+            .onChange(of: selectedDate) { _, _ in
+                reloadDisplayedCircles()
+            }
+            .onChange(of: searchTerm) { _, _ in
+                searchCircles()
             }
             .navigationDestination(for: ViewPath.self) { viewPath in
                 switch viewPath {
@@ -110,6 +107,31 @@ struct CirclesView: View {
                 default: Color.clear
                 }
             }
+        }
+    }
+
+    func reloadDisplayedCircles() {
+        if let selectedBlock {
+            var newDisplayedCircles = database.circles(in: selectedBlock).sorted(by: {$0.id < $1.id})
+            if let selectedDate {
+                newDisplayedCircles.removeAll(where: { $0.day != selectedDate.id })
+            }
+            displayedCircles = newDisplayedCircles
+        } else {
+            displayedCircles.removeAll()
+        }
+    }
+
+    func searchCircles() {
+        let searchTermLowercased = searchTerm.lowercased()
+        if searchTerm.trimmingCharacters(in: .whitespaces).count > 2 {
+            searchedCircles = database.eventCircles.filter({
+                $0.circleName.lowercased().contains(searchTermLowercased) ||
+                $0.circleNameKana.lowercased().contains(searchTermLowercased) ||
+                $0.penName.lowercased().contains(searchTermLowercased)
+            })
+        } else {
+            searchedCircles = nil
         }
     }
 }
