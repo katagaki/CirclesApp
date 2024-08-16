e5e60ab1854e0b2ac39f9a6eb6d6aae22c70f7db
katagaki <68231035+katagaki@users.noreply.github.com>
2024-08-16 11:05:21 +0900

    Improve login view

--- CiRCLES/Views/Login/LoginView.swift
+++ CiRCLES/Views/Login/LoginView.swift
@@ -15,40 +15,58 @@ struct LoginView: View {
     @State var isShowingDemoAlert: Bool = false
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 16.0) {
-            Text("Login.Title")
-                .fontWeight(.bold)
-                .font(.largeTitle)
-            Spacer()
-            HStack {
-                Image(systemName: "info.circle")
-                Text("Login.Disclaimer")
-                    .frame(maxWidth: .infinity, alignment: .leading)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16.0) {
+                Text("Login.Title")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundStyle(
+                        LinearGradient(colors: [.accent, .primary], startPoint: .leading, endPoint: .trailing)
+                    )
+                Spacer()
+                VStack(alignment: .leading, spacing: 32.0) {
+                    LoginFeatureHero(imageName: "Hero.Map",
+                                     title: "Hero.Map.Title",
+                                     description: "Hero.Map.Description")
+                    LoginFeatureHero(imageName: "Hero.Circles",
+                                     title: "Hero.Circles.Title",
+                                     description: "Hero.Circles.Description")
+                    LoginFeatureHero(imageName: "Hero.Favorites",
+                                     title: "Hero.Favorites.Title",
+                                     description: "Hero.Favorites.Description")
+                }
+                Spacer()
+                Divider()
+                HStack {
+                    Image(systemName: "info.circle")
+                    Text("Login.Disclaimer")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                Button {
+                    isShowingDemoAlert = true
+                } label: {
+                    Text("Shared.Login.Demo")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 6.0)
+                }
+                .buttonStyle(.bordered)
+                .clipShape(.capsule(style: .continuous))
+                Button {
+                    isAuthenticating = true
+                } label: {
+                    Text("Shared.Login")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom], 6.0)
+                }
+                .buttonStyle(.borderedProminent)
+                .clipShape(.capsule(style: .continuous))
             }
-            .font(.subheadline)
-            .foregroundStyle(.secondary)
-            Button {
-                isShowingDemoAlert = true
-            } label: {
-                Text("Shared.Login.Demo")
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity)
-                    .padding([.top, .bottom], 6.0)
-            }
-            .buttonStyle(.bordered)
-            .clipShape(.capsule(style: .continuous))
-            Button {
-                isAuthenticating = true
-            } label: {
-                Text("Shared.Login")
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity)
-                    .padding([.top, .bottom], 6.0)
-            }
-            .buttonStyle(.borderedProminent)
-            .clipShape(.capsule(style: .continuous))
+            .padding(18.0)
         }
-        .padding()
         .sheet(isPresented: $isAuthenticating) {
             SafariView(url: authManager.authURL)
                 .ignoresSafeArea()
