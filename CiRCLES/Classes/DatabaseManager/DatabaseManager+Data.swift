9aeb3dffe354160884946dadc22d133b1b4fd3ae
katagaki <68231035+katagaki@users.noreply.github.com>
2024-09-01 19:34:21 +0900

    Load hall and block mappings on appear if empty

--- CiRCLES/Classes/DatabaseManager/DatabaseManager+Data.swift
+++ CiRCLES/Classes/DatabaseManager/DatabaseManager+Data.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SQLite
 import SwiftData
+import UIKit
 
 extension DatabaseManager {
 
@@ -171,5 +172,75 @@ extension DatabaseManager {
             debugPrint(error.localizedDescription)
             return []
         }
+    }
+
+    // MARK: Common Images
+
+    func loadCommonImages() {
+        if let imageDatabase {
+            debugPrint("Loading common images")
+            do {
+                let table = Table("ComiketCommonImage")
+                let colName = Expression<String>("name")
+                let colImage = Expression<Data>("image")
+                var commonImages: [String: Data] = [:]
+                for row in try imageDatabase.prepare(table) {
+                    commonImages[row[colName]] = row[colImage]
+                }
+                self.commonImages = commonImages
+            } catch {
+                debugPrint(error.localizedDescription)
+            }
+        }
+    }
+
+    func coverImage() -> UIImage? { commonImage(named: "0001") }
+    func blockImage(_ blockID: Int) -> UIImage? { commonImage(named: "B\(blockID)") }
+    func jikoCircleCutImage() -> UIImage? { commonImage(named: "JIKO") }
+
+    func mapImage(for hall: ComiketHall, on day: Int, usingHighDefinition: Bool) -> UIImage? {
+        let mapImageNamePrefix = usingHighDefinition ? "LWMP" : "WMP"
+        let mapImageName = "\(mapImageNamePrefix)\(day)\(hall.rawValue)"
+        return commonImage(named: mapImageName)
+    }
+
+    func genreImage(for hall: ComiketHall, on day: Int, usingHighDefinition: Bool) -> UIImage? {
+        let genreImageNamePrefix = usingHighDefinition ? "LWGR" : "WGR"
+        let genreImageName = "\(genreImageNamePrefix)\(day)\(hall.rawValue)"
+        return commonImage(named: genreImageName)
+    }
+
+    // MARK: Circle Images
+
+    func loadCircleImages() {
+        if let imageDatabase {
+            debugPrint("Loading circle images")
+            do {
+                let table = Table("ComiketCircleImage")
+                let colID = Expression<Int>("id")
+                let colCutImage = Expression<Data>("cutImage")
+                var circleImages: [Int: Data] = [:]
+                for row in try imageDatabase.prepare(table) {
+                    circleImages[row[colID]] = row[colCutImage]
+                }
+                self.circleImages = circleImages
+            } catch {
+                debugPrint(error.localizedDescription)
+            }
+        }
+    }
+
+    func circleImage(for id: Int) -> UIImage? {
+        if let circleImageData = circleImages[id] {
+            return UIImage(data: circleImageData)
+        }
+        return nil
+    }
+
+    func commonImage(named imageName: String) -> UIImage? {
+        if let imageData = commonImages[imageName] {
+            return UIImage(data: imageData)
+        }
+        return nil
     }
 }
--- CiRCLES/Views/More/MoreDatabaseAdministratiion.swift
+++ CiRCLES/Views/More/MoreDatabaseAdministratiion.swift
@@ -57,7 +57,7 @@ struct MoreDatabaseAdministratiion: View {
                                         database.progressTextKey = "Shared.LoadingText.RepairingData"
                                     }
                                 }
-                                var actor = DataConverter(modelContainer: sharedModelContainer)
+                                let actor = DataConverter(modelContainer: sharedModelContainer)
                                 await actor.deleteAllData()
                                 await actor.loadAll(from: textDatabase)
                                 await MainActor.run {
