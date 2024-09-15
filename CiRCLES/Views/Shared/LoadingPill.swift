afee6b17598eb9dc28b267852b331e71470b2b53
katagaki <68231035+katagaki@users.noreply.github.com>
2024-09-15 13:53:05 +0900

    Allow using the app when loading favorites initially

--- CiRCLES/Views/Shared/LoadingPill.swift
+++ CiRCLES/Views/Shared/LoadingPill.swift
@@ -1,13 +1,13 @@
 //
-//  LoadingOverlay.swift
+//  LoadingPill.swift
 //  CiRCLES
 //
-//  Created by シン・ジャスティン on 2024/09/08.
+//  Created by シン・ジャスティン on 2024/09/15.
 //
 
 import SwiftUI
 
-struct LoadingOverlay: View {
+struct LoadingPill: View {
 
     @Environment(Database.self) var database
 
@@ -15,37 +15,33 @@ struct LoadingOverlay: View {
     @Binding var progressHeaderText: String?
 
     var body: some View {
-        ZStack {
-            Color.black.opacity(0.7)
-            VStack(spacing: 12.0) {
+        ZStack(alignment: .top) {
+            Color.clear
+            HStack(spacing: 4.0) {
+                ProgressView()
                 if progressHeaderText != nil || database.progressTextKey != nil {
-                    VStack(spacing: 6.0) {
+                    VStack(spacing: 2.0) {
                         if let progressHeaderText {
                             Text(NSLocalizedString(progressHeaderText, comment: ""))
+                                .font(.caption)
                                 .fontWeight(.bold)
                         }
                         if let progressTextKey = database.progressTextKey {
                             Text(NSLocalizedString(progressTextKey, comment: ""))
-                                .font(.body)
+                                .font(.caption2)
                                 .foregroundStyle(.secondary)
                         }
                     }
                 }
-                if database.isDownloading {
-                    ProgressView(value: database.downloadProgress, total: 1.0)
-                        .progressViewStyle(.linear)
-                } else {
-                    ProgressView()
-                }
             }
-            .padding()
-            .frame(maxWidth: .infinity)
-            .background(Material.regular)
-            .clipShape(RoundedRectangle(cornerRadius: 16.0))
-            .padding(32.0)
+            .padding([.top, .bottom], 4.0)
+            .padding([.leading, .trailing], 8.0)
+            .background(Color(uiColor: .secondarySystemBackground))
+            .clipShape(.capsule(style: .continuous))
+            .padding(8.0)
+            .shadow(radius: 3.0, y: 3.0)
             .matchedGeometryEffect(id: "LoadingWindow", in: namespace)
         }
-        .ignoresSafeArea()
-        .transition(.opacity.animation(.snappy.speed(2.0)))
+        .transition(.move(edge: .top).animation(.snappy.speed(2.0)))
     }
 }
