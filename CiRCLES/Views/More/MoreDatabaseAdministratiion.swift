fdcee8320aa6b14110c23463bbd8b14c25bf6af7
katagaki <68231035+katagaki@users.noreply.github.com>
2024-11-09 23:31:06 +0900

    Support managing downloaded data

--- CiRCLES/Views/More/MoreDatabaseAdministratiion.swift
+++ CiRCLES/Views/More/MoreDatabaseAdministratiion.swift
@@ -17,6 +17,8 @@ struct MoreDatabaseAdministratiion: View {
     @Environment(Database.self) var database
     @Environment(Oasis.self) var oasis
 
+    @State var files: [String: URL] = [:]
+
     @AppStorage(wrappedValue: false, "More.DBAdmin.SkipDownload") var willSkipDownload: Bool
 
     var body: some View {
@@ -31,9 +33,71 @@ struct MoreDatabaseAdministratiion: View {
                     }
                 }
             }
+            Section {
+                ForEach(files.keys.sorted(), id: \.self) { fileName in
+                    HStack {
+                        Text(fileName)
+                        Spacer()
+                        if let fileURL = files[fileName], let fileSizeString = fileSize(of: fileURL.path()) {
+                            Text(fileSizeString)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                            Button("Shared.Delete", role: .destructive) {
+                                if let fileURL = files[fileName] {
+                                    do {
+                                        try FileManager.default.removeItem(at: fileURL)
+                                        files.removeValue(forKey: fileName)
+                                    } catch {
+                                        debugPrint(error.localizedDescription)
+                                    }
+                                }
+                            }
+                        }
+                }
+            } header: {
+                ListSectionHeader(text: "More.DBAdmin.DownloadedData")
+            }
         }
         .navigationTitle("ViewTitle.More.DBAdmin")
         .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            refreshDownloadedDataList()
+        }
+    }
+
+    func refreshDownloadedDataList() {
+        var files: [String: URL] = [:]
+        if let documentsDirectoryURL,
+           let downloadedFiles = try? FileManager.default.contentsOfDirectory(
+            at: documentsDirectoryURL,
+            includingPropertiesForKeys: nil,
+            options: .skipsHiddenFiles
+           ) {
+            for file in downloadedFiles where file.isFileURL && file.pathExtension == "db" {
+                var fileName = file.lastPathComponent
+                fileName = fileName.replacingOccurrences(of: "webcatalog", with: "C")
+                fileName = fileName.replacingOccurrences(of: "Image1.db", with: "（画像データ）")
+                fileName = fileName.replacingOccurrences(of: ".db", with: "（テキストデータ）")
+                files[fileName] = file
+            }
+        }
+        withAnimation(.snappy.speed(2.0)) {
+            self.files = files
+        }
+    }
+
+    func fileSize(of path: String) -> String? {
+        do {
+            let attributes = try FileManager.default.attributesOfItem(atPath: path)
+            if let size = attributes[.size] as? UInt64 {
+                return ByteCountFormatter.string(fromByteCount: Int64(size), countStyle: .file)
+            }
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+        return nil
     }
 
     func repairData() async {
@@ -68,6 +132,7 @@ struct MoreDatabaseAdministratiion: View {
                 debugPrint(error.localizedDescription)
             }
         }
+        refreshDownloadedDataList()
         oasis.close()
         UIApplication.shared.isIdleTimerDisabled = false
     }
