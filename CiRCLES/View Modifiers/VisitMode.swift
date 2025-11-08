426eda2e959ef7ab62d8db3f32ca24a524cf928a
katagaki <68231035+katagaki@users.noreply.github.com>
2025-11-08 15:23:57 +0900

    Change how Visit Mode looks on iOS 26

---
+++ CiRCLES/View Modifiers/VisitMode.swift
@@ -0,0 +1,31 @@
+//
+//  VisitMode.swift
+//  CiRCLES
+//
+//  Created by シン・ジャスティン on 2025/11/08.
+//
+
+import SwiftUI
+
+struct VisitModeModifier: ViewModifier {
+    @Binding var isVisitModeOn: Bool
+
+    func body(content: Content) -> some View {
+        if #available(iOS 26.0, *) {
+            content
+                .presentationBackground(isVisitModeOn ? Color.accentColor : Color.clear)
+        } else {
+            content
+                .overlay {
+                    GradientBorder()
+                        .ignoresSafeArea(edges: .all)
+                }
+        }
+    }
+}
+
+extension View {
+    func visitModeStyle(_ isVisitModeOn: Binding<Bool>) -> some View {
+        self.modifier(VisitModeModifier(isVisitModeOn: isVisitModeOn))
+    }
+}
