1426d5f1a201c7b75f431e7c9a3f87534b654ae0
katagaki <68231035+katagaki@users.noreply.github.com>
2025-11-11 16:07:20 +0900

    Refactor map layers

--- CiRCLES/Views/Map/Layers/Favorites/MapFavoritesLayer.swift
+++ CiRCLES/Views/Map/Layers/Favorites/MapFavoritesLayer.swift
@@ -9,28 +9,109 @@ import SwiftUI
 
 struct MapFavoritesLayer: View {
 
-    @Binding var mappings: [LayoutCatalogMapping: [Int: WebCatalogColor?]]
-    var spaceSize: Int
+    @Environment(Favorites.self) var favorites
 
-    @Binding var width: Int
-    @Binding var height: Int
+    @Binding var canvasSize: CGSize
 
-    @AppStorage(wrappedValue: 1, "Map.ZoomDivisor") var zoomDivisor: Int
+    @Binding var mappings: [LayoutCatalogMapping: [Int]]
+    @State var favoriteMappings: [LayoutCatalogMapping: [Int: WebCatalogColor?]] = [:]
+
+    let spaceSize: Int
 
     var body: some View {
         ZStack(alignment: .topLeading) {
-            ForEach(Array(mappings.keys), id: \.hashValue) { layout in
+            ForEach(Array(favoriteMappings.keys), id: \.hashValue) { layout in
                 MapFavoriteLayerBlock(
                     layout: layout,
-                    colorMap: mappings[layout] ?? [:],
+                    colorMap: favoriteMappings[layout] ?? [:],
                     spaceSize: spaceSize
                 )
             }
             Color.clear
         }
-        .frame(
-            width: CGFloat(width / zoomDivisor),
-            height: CGFloat(height / zoomDivisor)
+        .frame(width: canvasSize.width, height: canvasSize.height)
+        .animation(.smooth.speed(2.0), value: canvasSize)
+        .allowsHitTesting(false)
+        .onChange(of: mappings) {
+            favoriteMappings.removeAll()
+            Task.detached {
+                await reloadFavorites()
+            }
+        }
+        .onChange(of: favorites.items) {
+            Task.detached {
+                await reloadFavorites()
+            }
+        }
+    }
+
+    func reloadFavorites() async {
+        let actor = DataFetcher(modelContainer: sharedModelContainer)
+
+        // Create favorites mapping for Web Catalog IDs
+        let layoutFavoriteWebCatalogIDMappings = mapFavoriteMappings(
+            mappings
         )
+        // List out all Web Catalog IDs
+        let webCatalogIDs: [Int] = Array(Set(
+            layoutFavoriteWebCatalogIDMappings.values
+                .reduce(into: [] as [Int]) { result, webCatalogIDs in
+                    result.append(contentsOf: webCatalogIDs.keys)
+                }
+        ))
+        // Map all Web Catalog IDs to space number suffixes
+        let spaceNumberSuffixes = await actor.spaceNumberSuffixes(
+            forWebCatalogIDs: webCatalogIDs
+        )
+        // Replace Web Catalog IDs with space number suffixes
+        let layoutFavoriteWebCatalogIDSorted = layoutFavoriteWebCatalogIDMappings
+            .reduce(into: [LayoutCatalogMapping: [Int: WebCatalogColor?]]()) { result, keyValue in
+                let mapping = keyValue.key
+                let colorMapping = keyValue.value
+                result[mapping] = colorMapping
+                    .reduce(into: [Int: WebCatalogColor?]()) { result, keyValue in
+                        let webCatalogID = keyValue.key
+                        let color = keyValue.value
+                        result[spaceNumberSuffixes[webCatalogID] ?? webCatalogID] = color
+                    }
+            }
+
+        await MainActor.run {
+            withAnimation(.smooth.speed(2.0)) {
+                self.favoriteMappings = layoutFavoriteWebCatalogIDSorted
+            }
+        }
+    }
+
+    func mapFavoriteMappings(
+        _ layoutWebCatalogIDMappings: [LayoutCatalogMapping: [Int]]
+    ) -> [LayoutCatalogMapping: [Int: WebCatalogColor?]] {
+        var layoutFavoriteWebCatalogIDMappings: [LayoutCatalogMapping: [Int: WebCatalogColor?]] = [:]
+        if let webCatalogIDMappedItems = favorites.wcIDMappedItems {
+            for (webCatalogID, favoriteItem) in webCatalogIDMappedItems {
+                // 1. Find the layout catalog mapping that matches the Web Catalog ID
+                let matchingLayoutMaps: [LayoutCatalogMapping: [Int]] = layoutWebCatalogIDMappings
+                    .filter { $1.contains(webCatalogID) }
+
+                // 2. Ensure that only one layout is selected
+                if let (matchedLayoutMap, _) = matchingLayoutMaps.first {
+                    // 3. Set up the sub-dictionary for the favorites mapping
+                    if layoutFavoriteWebCatalogIDMappings[matchedLayoutMap] == nil {
+                        let webCatalogIDs = layoutWebCatalogIDMappings[matchedLayoutMap] ?? []
+                        layoutFavoriteWebCatalogIDMappings[matchedLayoutMap] = webCatalogIDs
+                            .reduce(into: [:]) { partialResult, webCatalogID in
+                                let nilColor: WebCatalogColor? = nil
+                                partialResult[webCatalogID] = nilColor
+                            }
+                    }
+
+                    // 4. Set the color for the Web Catalog ID in the favorites mapping
+                    layoutFavoriteWebCatalogIDMappings[matchedLayoutMap]?[webCatalogID] = favoriteItem.favorite.color
+                }
+            }
+            // 5. Change keys to space sub number to force MapFavoriteLayerBlock to sort correctly
+            // TODO
+        }
+        return layoutFavoriteWebCatalogIDMappings
     }
 }
--- CiRCLES/Views/Map/Layers/MapLayer.swift
+++ CiRCLES/Views/Map/Layers/MapLayer.swift
@@ -9,23 +9,17 @@ import SwiftUI
 
 struct MapLayer: View {
 
+    @Binding var canvasSize: CGSize
     var image: UIImage
 
-    @Binding var width: Int
-    @Binding var height: Int
-
-    @AppStorage(wrappedValue: 1, "Map.ZoomDivisor") var zoomDivisor: Int
     @AppStorage(wrappedValue: true, "Customization.UseDarkModeMaps") var useDarkModeMaps: Bool
 
     var body: some View {
         Image(uiImage: image)
             .resizable()
-            .frame(
-                width: CGFloat(width / zoomDivisor),
-                height: CGFloat(height / zoomDivisor)
-            )
-            .animation(.smooth.speed(2.0), value: zoomDivisor)
             .colorInvert(adaptive: true, enabled: $useDarkModeMaps)
+            .frame(width: canvasSize.width, height: canvasSize.height)
+            .animation(.smooth.speed(2.0), value: canvasSize)
             .allowsHitTesting(false)
     }
 }
