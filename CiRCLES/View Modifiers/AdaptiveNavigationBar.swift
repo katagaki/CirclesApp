feaf4ef5d85b001533ad6ba096f97c04dfed3c6a
katagaki <68231035+katagaki@users.noreply.github.com>
2025-11-13 19:16:01 +0900

    Tweak map calculations

---
+++ CiRCLES/View Modifiers/AdaptiveNavigationBar.swift
@@ -0,0 +1,23 @@
+//
+//  AdaptiveNavigationBar.swift
+//  CiRCLES
+//
+//  Created by シン・ジャスティン on 2025/11/13.
+//
+
+import SwiftUI
+
+struct AdaptiveNavigationBarModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        if #unavailable(iOS 26.0) {
+            content
+                .toolbarBackground(.visible, for: .navigationBar)
+        }
+    }
+}
+
+extension View {
+    func adaptiveNavigationBar() -> some View {
+        self.modifier(AdaptiveNavigationBarModifier())
+    }
+}
