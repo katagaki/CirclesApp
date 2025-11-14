02d14876161158d9fbd1fa540cda1e91fae2f356
katagaki <68231035+katagaki@users.noreply.github.com>
2025-11-15 01:37:00 +0900

    Optimize highlightCircle

--- CiRCLES/Views/Map/MapView+Functions.swift
+++ CiRCLES/Views/Map/MapView+Functions.swift
@@ -81,68 +81,71 @@ extension MapView {
         }
     }
 
+    // swiftlint:disable function_body_length
     func highlightCircle(_ circle: ComiketCircle) async {
         let blockID = circle.blockID
         let spaceNumber = circle.spaceNumber
         let spaceNumberSuffix = circle.spaceNumberSuffix
-        
-        for (layout, webCatalogIDs) in mapper.layouts {
-            if layout.blockID == blockID && layout.spaceNumber == spaceNumber {
-                let zoomFactor = zoomFactorDouble(zoomDivisor)
-                let xMin: CGFloat = CGFloat(layout.positionX) / zoomFactor
-                let yMin: CGFloat = CGFloat(layout.positionY) / zoomFactor
-                let scaledSpaceSize = CGFloat(spaceSize) / zoomFactor
 
-                let count = webCatalogIDs.count
-                guard count > 0 else { return }
-
-                let needsReverse = layout.layoutType == .aOnBottom || layout.layoutType == .aOnRight
-
-                var circleIndex = spaceNumberSuffix
-
-                if needsReverse {
-                    circleIndex = count - 1 - spaceNumberSuffix
-                }
-
-                let countCGFloat = CGFloat(count)
-                let indexCGFloat = CGFloat(circleIndex)
-
-                let highlightRect: CGRect
-
-                switch layout.layoutType {
-                case .aOnLeft, .aOnRight, .unknown:
-                    let rectWidth = scaledSpaceSize / countCGFloat
-                    highlightRect = CGRect(
-                        x: xMin + indexCGFloat * rectWidth,
-                        y: yMin,
-                        width: rectWidth,
-                        height: scaledSpaceSize
-                    )
-                case .aOnTop, .aOnBottom:
-                    let rectHeight = scaledSpaceSize / countCGFloat
-                    highlightRect = CGRect(
-                        x: xMin,
-                        y: yMin + indexCGFloat * rectHeight,
-                        width: scaledSpaceSize,
-                        height: rectHeight
-                    )
-                }
-
-                let scrollPosition = CGPoint(
-                    x: highlightRect.midX,
-                    y: highlightRect.midY
-                )
-
-                await MainActor.run {
-                    mapper.popoverData = nil
-                    mapper.scrollToPosition = scrollPosition
-                    mapper.highlightData = HighlightData(
-                        sourceRect: highlightRect, shouldBlink: true
-                    )
-                }
-                return
+        guard let (layout, webCatalogIDs) = mapper.layouts.first(
+            where: { (layout: LayoutCatalogMapping, _) in
+                layout.blockID == blockID && layout.spaceNumber == spaceNumber
             }
+        ) else {
+            // TODO: Show message telling user the circle is not in this map
+            return
+        }
+
+        let zoomFactor = zoomFactorDouble(zoomDivisor)
+        let xMin: CGFloat = CGFloat(layout.positionX) / zoomFactor
+        let yMin: CGFloat = CGFloat(layout.positionY) / zoomFactor
+        let scaledSpaceSize = CGFloat(spaceSize) / zoomFactor
+
+        let count = webCatalogIDs.count
+        guard count > 0 else { return }
+
+        var circleIndex = spaceNumberSuffix
+
+        if layout.layoutType == .aOnBottom || layout.layoutType == .aOnRight {
+            circleIndex = count - 1 - spaceNumberSuffix
+        }
+
+        let countCGFloat = CGFloat(count)
+        let indexCGFloat = CGFloat(circleIndex)
+
+        let highlightRect: CGRect
+        switch layout.layoutType {
+        case .aOnLeft, .aOnRight, .unknown:
+            let rectWidth = scaledSpaceSize / countCGFloat
+            highlightRect = CGRect(
+                x: xMin + indexCGFloat * rectWidth,
+                y: yMin,
+                width: rectWidth,
+                height: scaledSpaceSize
+            )
+        case .aOnTop, .aOnBottom:
+            let rectHeight = scaledSpaceSize / countCGFloat
+            highlightRect = CGRect(
+                x: xMin,
+                y: yMin + indexCGFloat * rectHeight,
+                width: scaledSpaceSize,
+                height: rectHeight
+            )
+        }
+
+        let scrollPosition = CGPoint(
+            x: highlightRect.midX,
+            y: highlightRect.midY
+        )
+
+        await MainActor.run {
+            mapper.popoverData = nil
+            mapper.scrollToPosition = scrollPosition
+            mapper.highlightData = HighlightData(
+                sourceRect: highlightRect, shouldBlink: true
+            )
         }
     }
+    // swiftlint:enable function_body_length
 
 }
